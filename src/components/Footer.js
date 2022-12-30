7fdbf432d9ca2d9fbe3e80e5f8f0d76f12a6d963
Franco Yunes <franco.yunes@hotmail.com>
2022-12-30 00:23:19 -0300

    :sparkles: Meme Generator Codo a Codo

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+function Footer() {
+    return (
+        <footer>
+            <p>               
+                <a href="https://www.linkedin.com/in/francoyunes/">
+                    <img className="logo" src="https://cdn.discordapp.com/attachments/781224167524925460/1058183862183809084/linkedin.png" alt="Linkedin"/>
+                </a>
+                <a href="https://www.linkedin.com/in/francoyunes/">
+                    <img className="logo" src="https://cdn.discordapp.com/attachments/781224167524925460/1058184365319929946/github-sign.png" alt="Linkedin"/>
+                </a>
+            </p>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+
+function Header() {
+    return (
+        <header className="header">            
+            <h2 className="header-title">MEME GENERATOR</h2>
+        </header>
+    );
+}
+
+export default Header;
